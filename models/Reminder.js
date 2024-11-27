06af85b890105f0b65687e34188333d7ccc319ac
NoPref <mrrmayvk@gmail.com>
2024-11-27 18:10:33 +0300

    repeat function for reminders

--- models/Reminder.js
+++ models/Reminder.js
@@ -5,6 +5,11 @@ const reminderSchema = new mongoose.Schema({
   description: String,
   date: Date,
   isNotified: { type: Boolean, default: false },
+  repeat: {
+    type: String,
+    enum: ['None', 'Daily', 'Weekly', 'Monthly', 'Yearly'], // Define valid repeat options
+    default: 'None',
+  },
 });
 
 module.exports = mongoose.model('Reminder', reminderSchema);
--- routes/reminders.js
+++ routes/reminders.js
@@ -5,15 +5,24 @@ const Reminder = require('../models/Reminder');
 
 // Create a new reminder
 router.post('/', async (req, res) => {
-  const reminder = new Reminder(req.body);
-  await reminder.save();
-  res.status(201).send(reminder);
+  try {
+    const { title, description, date, isNotified, repeat } = req.body;
+    const reminder = new Reminder({ title, description, date, isNotified, repeat });
+    await reminder.save();
+    res.status(201).send(reminder);
+  } catch (error) {
+    res.status(400).send({ error: 'Error creating reminder', details: error.message });
+  }
 });
 
 // Get all reminders
 router.get('/', async (req, res) => {
-  const reminders = await Reminder.find();
-  res.send(reminders);
+  try {
+    const reminders = await Reminder.find();
+    res.send(reminders);
+  } catch (error) {
+    res.status(500).send({ error: 'Error fetching reminders' });
+  }
 });
 
 // Update a reminder
